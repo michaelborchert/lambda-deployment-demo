8e34fbc5f06b4dd2bcc8262e94b89afc63b22787
Michael Borchert <mborch@amazon.com>
2019-01-13 21:35:18 +0700

    making a change.

--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,8 @@ import json
 def lambda_handler(event, context):
     if event:
         return json.dumps(event)
-        
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello, from Lambda!')
     }
